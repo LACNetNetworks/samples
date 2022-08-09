1a6ef8a56f055f9b227e39038cd5b857500ef053
Eduardo Marchena <edumar111@gmail.com>
2022-08-09 15:40:50 -0500

    update private channel local

--- tessera/keys.js
+++ tessera/keys.js
@@ -1,33 +1,34 @@
 module.exports = {
   enclave: {
     node1: {
-      publicKey: "t2c4rpELeEbg5O2DsJ1Qhs4/osspJBs32On6cuhpNQs=",
+      publicKey: "lv491VzucfExUtsU0+1RTmYD/8ZmSXreY+eRZGDhjHg=",
     },
     node2: {
-      publicKey: "Dz9W4knLay2FBctOzb8IsgfrafdHauIv/LVpqLEmP1Y=",
+      publicKey: "BhaooaP1pkeHKmw9X9gBrznC7ieH8gmX6XQXYLIKwyM=",
     },
     node3: {
-      publicKey: "pTRmdREaHCb4Ew2P1TSUGnKxob6Mj+ldfXwYIret/Hs=",
+      publicKey: "4iebmz+Tu/L1ripJVhP9yp0CmkUmcyMM/YzwQSRhqHY=",
     },
   },
   network: {
     node1: {
-      url: "http://localhost:8545",
-      wsUrl: "ws://localhost:9546",
+      url: "http://localhost:4545",
+      wsUrl: "ws://localhost:5546",
       privateKey:
-        "ca71103d43febee14591632c74e26499db75536352d6c9997377944bed4db328",
+        "a65d61deb3d18b44dd4b3499c3741ca3a94a08487a987bd6f9759ad1393959ee",
+        
     },
     node2: {
-      url: "http://localhost:8546",
-      wsUrl: "ws://localhost:9547",
+      url: "http://localhost:4546",
+      wsUrl: "ws://localhost:5547",
       privateKey:
-        "ffaf4ccbc6ccf794f1779635758396ab8264dd26b0146fb47e05d05b8894ce60",
+        "0e179c79e14b172f8b893afa1acc5036f84e116388fd10c3e07b6be55171f0ab",
     },
     node3: {
-      url: "http://localhost:8547",
-      wsUrl: "ws://localhost:9548",
+      url: "http://localhost:4547",
+      wsUrl: "ws://localhost:5548",
       privateKey:
-        "653507c77e2ab09fe673dfa9c21644bc0e0e5607d5bfd7a51f55dc2a4ecd03ff",
+        "c75101051883984991d327343e191f6a986178d6c7448c22c99d0695d5e490a7",
     },
   },
 };
